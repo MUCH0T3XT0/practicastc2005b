69d2687ec2ad73e4f603f52d969d188a1c8f0dbf
MUCH0T3XT0 <A01276139@tec.mx>
2024-04-29 22:55:30 -0600

    Se agrega el laboratorio 6

--- Laboratorio6/Lab6.js
+++ Laboratorio6/Lab6.js
@@ -1,65 +1,36 @@
 
-//Imprimir document
-console.log(document)
 
-//getElementById()
-const paragraph1 = document.getElementById("para1");
-console.log(paragraph1);
-console.log(paragraph1.textContent); //Obtener solo el texto
-
-//querySelector()
-const h1Element = document.querySelector("h1"); //Etiquetas generales
-console.log(h1Element);
-
-const list = document.querySelector(".list"); //Clases
-console.log(list);
-
-//querySelectorAll()
-
-const listItems = document.querySelectorAll("ul > li");
-console.log(listItems); 
-
-const listaDeElementos = document.querySelectorAll("ul > li");
-
-listaDeElementos.forEach((item) => {
-  console.log(item);
-});
-
-//createElement() y appendChild()
+//Separador
+let separador = document.createElement("hr");
+document.body.appendChild(separador);
+//Titulo
+let elementoTitulo = document.createElement("h1");
+elementoTitulo.textContent = "Preguntas";
+document.body.appendChild(elementoTitulo);
+//Preguntas del laboratorio
 let listaSinOrden = document.createElement("ul");
 document.body.appendChild(listaSinOrden);
-
 let elemento1Lista = document.createElement("li");
-elemento1Lista.textContent = "HTML";
+elemento1Lista.textContent = "¿Por qué es una buena práctica usar JavaScript para checar que sean válidos los inputs de las formas antes de enviar los datos al servidor?";
+let elemento4Lista = document.createElement("p");
+elemento4Lista.textContent = "Si, debido a que puede mejorar aspectos como la seguridad, eficiencia experiencia de usuario. Al validar las entradas antes de enviarselos al servidor, se puede reducir la cantidad de solicitudes hechas al servidor.";
 listaSinOrden.appendChild(elemento1Lista);
+listaSinOrden.appendChild(elemento4Lista);
+
 let elemento2Lista = document.createElement("li");
-elemento2Lista.textContent = "CSS";
+elemento2Lista.textContent = "¿Cómo puedes saltarte la seguridad de validaciones hechas con JavaScript?";
+let elemento5Lista = document.createElement("p");
+elemento5Lista.textContent = "Modificando el DOM se pueden eludir estas validaciones al eliminar o modificar los elementos del formulario. Igualmente es posible enviar datos directamente al servidor por medio de Postman por ejemplo";
+
 listaSinOrden.appendChild(elemento2Lista);
+listaSinOrden.appendChild(elemento5Lista);
+
+
 let elemento3Lista = document.createElement("li");
-elemento3Lista.textContent = "JS";
+elemento3Lista.textContent = "Si te puedes saltar la seguridad de las validaciones de JavaScript, entonces ¿por qué la primera pregunta dice que es una buena práctica?";
+let elemento6Lista = document.createElement("p");
+elemento6Lista.textContent = "Siguen proporcionando una mejor experiencia al usuario, ofrecindole retroalimentacion inmediata y ayudandolo a evitar errores. Tambien puede evitar sobrecargar el servidor al enviar datos obviamente incorrectos";
 listaSinOrden.appendChild(elemento3Lista);
+listaSinOrden.appendChild(elemento6Lista);
 
 document.body.appendChild(document.createElement("hr"));
-
-//Style
-let newH3 = document.createElement("h3");
-newH3.textContent = "Hola soy un texto en color negro"
-document.body.appendChild(newH3);
-
-const h3 = document.querySelector("h3");
-h3.style.color = "blue";
-h3.textContent = "Ohh no me cambiaron a azul."
-console.log(newH3)
-
-
-//EventListener
-let newButton = document.createElement("button");
-newButton.textContent = "Mostrar Alerta"
-newButton.setAttribute("id","btn");
-document.body.appendChild(newButton);
-
-const button = document.getElementById("btn");
-
-button.addEventListener("click", () => {
-    alert("Gracias por el click");
-});
